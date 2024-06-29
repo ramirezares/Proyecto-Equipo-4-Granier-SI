dadfebcc1b4f9b4f21e798c3cc18f92e325891b2
Daniela Zambrano <d.zambrano@correo.unimet.edu.ve>
2024-06-29 13:02:51 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,14 @@
-//import React from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Sugestions from './pages/Sugestions'
 
-//import { WebRouter } from './router/WebRouter.jsx'
-//import {RouterProvider} from "react-router-dom"
-//import Register from './pages/Register.jsx'
+import { WebRouter } from './router/WebRouter.jsx'
+import {RouterProvider} from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <Sugestions></Sugestions>
+    <React.StrictMode>
+    <RouterProvider router={WebRouter}/>
+    </React.StrictMode>
 )
 
-/*<React.StrictMode>
-    <RouterProvider router={WebRouter}/>
-  </React.StrictMode>*/
+/**/
