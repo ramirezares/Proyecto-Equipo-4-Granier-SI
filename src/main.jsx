aa493d94755db9f4d4e83da8d027f2ad2bd057fd
Daniela Zambrano <d.zambrano@correo.unimet.edu.ve>
2024-06-30 15:24:47 -0400

    ProductSetting

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+//import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { WebRouter } from "./router/WebRouter.jsx";
-import { RouterProvider } from "react-router-dom";
-import { UserProvider } from "./hooks/UserContext"; // Ajusta la ruta si es necesario
+import ProductSetting from "./pages/ProductSetting";
+//import { WebRouter } from "./router/WebRouter.jsx";
+//import { RouterProvider } from "react-router-dom";
+//import { UserProvider } from "./hooks/UserContext"; // Ajusta la ruta si es necesario
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <ProductSetting></ProductSetting> 
+);
+
+/*
+<React.StrictMode>
     <UserProvider>
       <RouterProvider router={WebRouter} />
     </UserProvider>
   </React.StrictMode>
-);
+*/
