f19cf699493463f004571448de44a9ce328b6335
Diego Mendez <d.mendez@correo.unimet.edu.ve>
2024-07-01 23:31:04 -0400

    Reviwe card responsive

--- src/components/SimpleReviewCard.jsx
+++ src/components/SimpleReviewCard.jsx
@@ -1,28 +1,29 @@
 import PropTypes from 'prop-types';
 
 const Comment = ({ comment }) => {
-    return (
-        <div className="container border border-grey-300 rounded flex mb-4">
-            <div className="w-1/4 flex flex-col justify-center items-center">
-            <img
-                src={comment.photo}
-                alt={comment.username}
-                className="w-12 h-12 m-5 rounded-full mx-auto"
-            /></div>
-            <div className="w-3/4 ">
-                <h5 className="text-lg font-bold mt-3">{comment.username}</h5>
-                <p className="text-gray-600">{comment.text}</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="container border border-grey-300 rounded mb-4 md:flex">
+      <div className="w-1/4 md:w-1/4 flex justify-center items-center mb-4 md:mb-0">
+        <img
+          src={comment.photo}
+          alt={comment.username}
+          className="w-12 h-12 m-5 rounded-full mx-auto"
+        />
+      </div>
+      <div className="w-3/4 md:w-3/4 ml-8 mb-4">
+        <h5 className="text-lg font-bold ">{comment.username}</h5>
+        <p className="text-gray-600 md:mt-0 mt-4">{comment.text}</p>
+      </div>
+    </div>
+  );
 };
 
 Comment.propTypes = {
-    comment: PropTypes.shape({
-        photo: PropTypes.string.isRequired,
-        username: PropTypes.string.isRequired,
-        text: PropTypes.string.isRequired,
-    }).isRequired,
+  comment: PropTypes.shape({
+    photo: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default Comment;
