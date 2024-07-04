010a92fbdd570d1ca93433fbd4c10843962eda77
Daniela Zambrano <d.zambrano@correo.unimet.edu.ve>
2024-07-03 21:06:42 -0400

    Sugestion conecction email

--- src/pages/Sugestions.jsx
+++ src/pages/Sugestions.jsx
@@ -1,4 +1,42 @@
+import { useState } from "react";
+import { useUser } from "../hooks/useUser";
+import emailjs from '@emailjs/browser';
+
+
 const Sugestions = () => {
+    const {userData} = useUser();
+
+    const[email, setEmail]=useState('');
+    const[message, setMessage]=useState('');
+    const[concept, setConcept]=useState('');
+
+    setEmail(userData.email);
+
+    const handleSubmit =(e)=>{
+        e.preventDefault
+        const serviceId='service_ivg35m1';
+        const templateId='template_suvzcia';
+        const publicKey= 'akedPK6z4qVAyGKbi';
+
+        const templateParams={
+            message:message,
+            concept:concept,
+            from_email:email
+        }
+
+
+        emailjs.send(serviceId, templateId, templateParams, publicKey)
+        .then((response) => {
+            console.log('SUCCESS!', response);
+            setMessage('');
+            setConcept('');
+        })
+        .catch((error) => {
+            console.log('FAILED...', error.text);
+        })
+
+    }
+
 
     return (
 
@@ -7,15 +45,19 @@ const Sugestions = () => {
             <hr className="h-px mb-5 bg-gray-300 border-0" />
             <p className="text-lg">En esta sección usted podrá enviarnos la retroalimentación necesaria para poder mejorar su experiencia en Granier.</p>
             
-            <form onSubmit={null}>
+            <form id="form" onSubmit={handleSubmit}>
+
                 {/*Input Pequeño */}
                 <div className="block mb-6">
                     <label className="block mb-2 text-sm font-medium text-gray-900">Concepto</label>
                     <input 
+                        name="concept"
                         type="text" 
                         id="first_name" 
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" 
-                        placeholder="..." 
+                        placeholder="..."
+                        value={concept}
+                        onChange={(e) => setConcept(e.target.value)}
                         required />
                 </div>
 
@@ -23,10 +65,13 @@ const Sugestions = () => {
                 <div className="block mb-6">
                 <label className="block mb-2 text-sm font-medium text-gray-900 ">Cuéntanos</label>
                 <textarea 
+                    name="message"
                     id="message" 
                     rows="4" 
+                    value={message}
                     className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" 
                     placeholder="Escribe lo que pienses aquí..." 
+                    onChange={(e) => setMessage(e.target.value)}
                     required
                     />
                 </div>
@@ -34,8 +79,10 @@ const Sugestions = () => {
                 {/*Boton*/}
                 <div className="block mb-4 text-right">
                     <button
+                        id="button"
                         className="bg-[#AD986E] hover:bg-[#AD986E] text-white font-bold py-2 px-4 rounded"
                         type="submit"
+                        value="Submit"
                     >Enviar</button>
                 </div>
             </form>
